0380d1f0b287358b521569e11b627fb09c051a4f
Henrry Bran <2168036320108@ingenieria.usac.edu.gt>
2024-06-22 10:22:52 -0600

    add
    se agrego el script de la bd, y el backend

---
+++ Proyecto/Backend/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const db = require('./db');
+const cors = require('cors');
+const morgan = require('morgan'); // Importa morgan
+
+const app = express();
+
+// Middleware para habilitar CORS (Cross-Origin Resource Sharing)
+app.use(cors());
+
+// Middleware de logging con morgan
+app.use(morgan('dev'));
+
+// Endpoint para llamar al procedimiento almacenado (GET)
+app.get('/grafico', async (req, res) => {
+    try {
+        const connection = await db.getConnection();
+        const [rows] = await connection.execute('CALL GetTopMemoryProcesses();');
+        connection.release();
+        res.json(rows); // asumiendo que el quinto resultado es el total
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error al ejecutar el procedimiento almacenado.' });
+    }
+});
+
+// Endpoint para llamar a la función definida (GET)
+app.get('/tabla', async (req, res) => {
+    try {
+        const connection = await db.getConnection();
+        const [rows] = await connection.execute('CALL ConvertirMemoriaToMB();');
+        connection.release();
+        res.json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error al ejecutar el procedimiento almacenado.' });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en puerto ${PORT}`);
+});
